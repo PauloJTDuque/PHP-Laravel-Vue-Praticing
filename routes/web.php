6207bc8c00ed1ae832a50d76abcb21ac258a2d8d
Paulo Duque <paulo.duque@terra.com.br>
2023-08-12 10:23:24 -0300

    Routes  120823

--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,32 @@ Route::get('/', function () {
     return 'Sobre nós';
 });*/
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return 'Login'; })->name('site.login');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos'; })->name('app.produtos');
+});
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/rota1', function() {
+    echo 'Rota 1';    
+})->name('site.rota1');
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+// Route::redirect('rota2', '/rota1');
+
+
 // nome, categoria, assunto, mensagem
 
 
-Route::get(
+/*Route::get(
     '/contato/{nome}/{categoria_id}',
     function(
         string $nome = 'Desconhecido',
